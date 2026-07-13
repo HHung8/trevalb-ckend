495d5cf2e4fed48358b565444d2df76ab1c564e7
hung <hung@hungs-MacBook-Air.local>
2026-07-13 12:56:57 +0700

    add file modifile

--- obj/Debug/net8.0/TrevalApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrevalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrevalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d499f0c9e84d2d46b03d2b62ba61a4ec0c59cafe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61568e1609b5f5eccf03d96bad4f7a7dd8589497")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrevalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrevalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
